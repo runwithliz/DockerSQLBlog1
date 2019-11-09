721c53c662714eb9f7c4a871c02ddacfa2109394
runwithliz <57550734+runwithliz@users.noreply.github.com>
2019-11-09 16:27:41 -0600

    AlternateStatus

---
+++ checkstatus.sh
@@ -0,0 +1,16 @@
+echo "Checking SQL Server"
+STATUS=$(/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P VeRYSECR3T! -d master -Q "SET NOCOUNT ON; SELECT 1" -W -h-1 )
+
+
+while [ "$STATUS" != 1 ]
+do
+sleep 1s
+
+echo "Checking SQL Server"
+STATUS=$(/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P VeRYSECR3T! -d master -Q "SET NOCOUNT ON; SELECT 1" -W -h-1 )
+done
+
+
+echo "SQL UP!"
+
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P VeRYSECR3T! -d master -Q "CREATE DATABASE SERVERUP" -W -h-1 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #start SQL Server, start the script to create/setup the DB
- /init.sh & /opt/mssql/bin/sqlservr
+ /checkstatus.sh & /opt/mssql/bin/sqlservr
